3e7390bed4c80a351601387b8a1d819c03b1b8f4
Raihan Uddin <rkrazzkhan01731@gmail.com>
2025-04-23 19:32:54 +0800

    deply for check routing

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router";
 import Navbar from "../Navbar/Navbar";
-import Footer from "../Footer/Footer";
 
 const NotFound = () => {
   return (
